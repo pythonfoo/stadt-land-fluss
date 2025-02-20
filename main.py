ba2bd19da561f5011318bb3d7166083c2eca9a8b
Niklas Sombert <niklas@ytvwld.de>
2025-02-20 18:24:36 +0100

    bessere Fehlerbehandlung bei der Abfrage der Anzahl

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import random
 import string
 import wikidata
 
-spieler_anzahl = int(input("Wie viele wollen spielen? "))
+while True:
+    try:
+        spieler_anzahl = int(input("Wie viele wollen spielen? "))
+    except ValueError:
+        print("Du musst eine Zahl eingeben!")
+        continue
 
-if spieler_anzahl > 10:
-    print("Es können maximal 10 Spieler*innen teilnehmen!")
-    exit()
+    if spieler_anzahl > 10:
+        print("Es können maximal 10 Spieler*innen teilnehmen!")
+        continue
+    if spieler_anzahl <= 0:
+        print("Gib bitte eine positive Zahl ein!")
+        continue
+    
+    break
 
 print("Lade Daten...")
 städte = wikidata.get_item_names_for_category("Q1549591")  # Großstadt
